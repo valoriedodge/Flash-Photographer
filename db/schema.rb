cd84bb4651154f820472e8015b2c1e92e32c7a32
vjdodge <valoriejones@gmail.com>
2016-06-13 22:19:15 -0700

    locations moved

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611145046) do
+ActiveRecord::Schema.define(version: 20160605201229) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "street"
